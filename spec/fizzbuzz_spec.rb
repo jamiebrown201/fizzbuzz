d47439084eea74afcb0005b5ded9927dea61f0b0
“DovileSand” <dovile.sand@protonmail.ch>
2015-11-20 17:10:25 +0000

    Writes a test for multiples of 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,7 +3,11 @@ require 'fizzbuzz'
 describe 'fizzbuzz' do
 
     it 'returns "Fizz" when passed a number that is a multiple of 3' do
-        expect(fizzbuzz(3)).to eq("Fizz")
+      expect(fizzbuzz(3)).to eq("Fizz")
     end
-    
+
+    it 'returns "Buzz" when passed a number that is a multiple of 5' do
+      expect(fizzbuzz(5)).to eq("Buzz")
+    end
+
 end
